388072f54871e6861ebf672eec2390df66b712e6
John Carges <johncarges@gmail.com>
2023-08-21 12:59:02 -0400

    passes all tests

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
+    for i in range(10,0,-1):
+        print(i)
+    print("Happy New Year!")
     pass
 
 def square_integers(int_list):
-    # code goes here!
+    return [i**2 for i in int_list]
     pass
 
 def fizzbuzz():
-    # code goes here!
+    for i in range(1,101):
+        string = ""
+        if i%3 == 0:
+            string += "Fizz"
+        if i%5 == 0:
+            string += "Buzz"
+        if string == "":
+            print(i)
+        else:
+            print(string)    
     pass
